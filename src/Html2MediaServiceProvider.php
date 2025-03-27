b5df786be3d8227590250c157b7b6e264820a713
L’Institut Agro Enseignement à distance <126262543+institut-agro-ead@users.noreply.github.com>
2025-03-27 14:41:38 +0100

    Upgrading Html2pdf CDN version

--- src/Html2MediaServiceProvider.php
+++ src/Html2MediaServiceProvider.php
@@ -21,7 +21,7 @@ class Html2MediaServiceProvider extends PackageServiceProvider
     public function packageBooted()
     {
         FilamentAsset::register([
-            Js::make('html2pdf', 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js'),
+            Js::make('html2pdf', 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'),
             Js::make('html2pdf-script', __DIR__ . '/../resources/js/html2pdf.js'),
         ], 'html2media');
     }
