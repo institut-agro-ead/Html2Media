b2bf383ff879b836719b681033ec7a0fad87f6cc
L’Institut Agro Enseignement à distance <126262543+institut-agro-ead@users.noreply.github.com>
2025-06-16 11:56:48 +0200

    Forcing print content to rerender

--- resources/views/tables/actions/html-2-media-table-action.blade.php
+++ resources/views/tables/actions/html-2-media-table-action.blade.php
@@ -1,5 +1,5 @@
 <div style="display: {{ isset($elementId) ? 'none' : 'block' }}">
-    <main id="print-smart-content-{{ $elementId ?? '' }}" style="color: black;" wire:ignore>
+    <main id="print-smart-content-{{ $elementId ?? '' }}" style="color: black;" wire:replace>
         {!! $content !!}
         <iframe style="display: none" id="print-smart-iframe-{{ $elementId ?? '' }}"></iframe>
     </main>
